dc1ea59a8bcce57ddccc31fe93a7fe904b8d4cef
Alisa Siegmund <alisa.siegmund@gmail.com>
2020-10-01 20:43:48 +0200

    wip

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -7,8 +7,26 @@ class ContributionsController < ApplicationController
   end
 
   def create
+      @contribution = Contribution.new(contribution_params)
+      @issue = Issue.find(params[:issue_id])
+      # @issue = Issue.find(params[:issue_id])
+    if @contribution.save
+      redirect_to issue_path
+    else
+      render :new
+    end
+
   end
 
   def destroy
   end
+
+
+  private
+
+  def contribution_params
+    params.require(:contribution).permit(:contribution_type, :contribution_content, :public, :author, :email)
+
+  end
+
 end
