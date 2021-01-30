14b044ee9a99ce8791ca5789b0583742a6e950e1
Alisa Siegmund <alisa.siegmund@gmail.com>
2021-01-30 23:32:20 +0100

    added validations on contribution model

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,5 +1,9 @@
 class Contribution < ApplicationRecord
   belongs_to :issue
   has_one_attached :photo
+  validates :con_type, presence: true
+  validates :content, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
 
 end
